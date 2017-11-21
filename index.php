d819eb2cee46c69d77636f1d0c5598dc42730c79
LeonTeixeira <leon.cruz.teixeira@gmail.com>
2017-11-21 10:52:56 -0200

    Páginas iniciais

---
+++ index.php
@@ -0,0 +1,31 @@
+<?php
+    session_start();
+
+    require ('..\conexao.php');
+
+    $matricula = $_POST['matricula'];
+    $senha = MD5($_POST['senha']);
+    $_SESSION['matricula'] = $matricula;
+
+    $banco = conectarBanco();
+
+    $buscar = "SELECT * FROM funcionarios WHERE matricula = '$matricula' AND senha = '$senha'";
+    $resultado = mysqli_query($banco, $buscar);
+
+    if(mysqli_num_rows($resultado) == 1){
+        $tupla = mysqli_fetch_assoc($resultado);
+        if($tupla['cargo'] == 'gerente'){
+            $_SESSION['cargo'] = $tupla['cargo'];
+            header('Location:..\Gerente\tela-gerente.php');
+        } else {
+            header('Location:..\Funcionario\tela-funcionarios.php');
+        }
+    } else {
+        echo '<script>
+                alert("Matrícula ou senha estão incorretos");
+                history.back();
+              </script>';
+    }
+
+    mysqli_close($banco);
+ ?>
