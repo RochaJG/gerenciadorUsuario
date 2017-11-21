8c5369bf5633d9fd09b47e113eafb6168624b403
LeonTeixeira <leon.cruz.teixeira@gmail.com>
2017-11-21 10:32:28 -0200

    Exibição da hora para o gerente

--- Gerente/tela-gerente.php
+++ Gerente/tela-gerente.php
@@ -80,4 +80,34 @@
          ?>
     </body>
 
+    <script type="text/javascript">
+        let time = function(){
+            setInterval(function(){
+                let data = new Date();
+                let dia = data.getDate();
+                let dia_semana = data.getDay();
+                let mes = data.getMonth();
+                let hora = data.getHours();
+                let minutos = data.getMinutes();
+                let segundos = data.getSeconds();
+
+                let horario = dias_semana[dia_semana] + ' ' + dia + ' ' + 'de ' + meses[mes]
+                              + 'às ' + hora + ':' + minutos + ':' + segundos;
+
+                document.getElementById('hora').innerHTML = horario;
+            }, 1000);
+        }
+
+        time();
+
+        let dias_semana = {
+            0:'Domingo', 1: 'Segunda', 2: 'Terça', 3:'Quarta', 4:'Quinta', 6:'Sexta', 7:'Sábado'
+        }
+        let meses = {
+            0:'Janeiro', 1:'Fevereiro', 2:'Março', 3:'Abril', 4:'Maio', 5:'Junho',
+            6:'Julho', 7:'Agosto', 8:'Setembro', 9:'Outubro', 10:'Novembro', 11:'Dezembro'
+        };
+
+    </script>
+
     </html>
